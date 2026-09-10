44da3df732dfb8acce9ad8b348debc9d0b73230e
AsifHassanjs <dasif489@gmail.com>
2026-09-10 23:36:46 +0530

    just basic which i already knew

---
+++ basics/01basic.py
@@ -0,0 +1,10 @@
+# python is general purpose but very easy programming language
+# it is used to develop almost every kind of application like Gen AI,web app,desktop,Data science ,iot etc
+#python is not used to develop system level applications(os and drivers) and  mobile applications .
+# Developed by Guido van Rossum, a Dutch scientist who is phd in maths.
+# inspired by power of c lang and simplicity of ABC lang.
+# it is simple (swap 2 numbers a,b=b,a done) ,dynamically typed,  supports multiple programming paradigms (supports both procedure oriented like c language and object oriented like java), compiled(fastlike c and c++) as well as interpreted(slow) ,cross platform ,extensible and has huge library.
+# python compiler+python interpreter=implementation(26+ implementations  available for python like cpython(we use often),jpython,pypy(fast as uses just in time compilation) etc)
+# The word implementation means the tools/software which are used for the execution of programs written in python language.
+# source code(code we write in any language ) is executed by os and cpu 
+# machine/platform/environment is environment where program is being run like operating system(like linux, mac, windows etc) and cpu.
