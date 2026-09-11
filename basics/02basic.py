9d45e2c108242d9fda37dc038e503d3636275c6d
AsifHassanjs <dasif489@gmail.com>
2026-09-11 10:19:45 +0530

    numeric

---
+++ basics/02basic.py
@@ -0,0 +1,50 @@
+# import sys;
+# both data types as well as szie of data types are dynamically managed
+# data types in python are also unbounded
+# a=10;
+# print(sys.getsizeof(a));
+# print(type(a));
+# a=1655657686565787;
+# print(sys.getsizeof(a)); 
+# a="sghvgv hjbhjvvghbxhv";
+# print(sys.getsizeof(a)); 
+# print(type(a));
+# data types classified in 6 catagories
+# 1=Numeric(int float complex) can be represented in 4 ways decimal octal binary hexadecimal
+# a=10;
+# print(a);
+# ab=-0b11;
+#binary
+# print(ab);
+# a=0o234;
+#octal
+# print(a);
+# das=0xa23f;
+#hexa
+# print(das);
+# base conversion function
+# used to convert from one from to another but gives in string form;
+# a=5; 
+# x=bin(a);
+# print(x);
+# print(type(x)); 
+# a=0xf5; 
+# x=bin(a);
+# print(x);
+# print(type(x)); 
+# a=5; 
+# x=oct(a);
+# print(x);
+# print(type(x));  
+# a=5; 
+# x=hex(a);
+# print(x);
+# print(type(x)); 
+# float supports only decimal number system and  3.5e2=3.5*100
+#print(10/5);
+#/ in python is always float division 
+# complex
+# a=3+5j;
+# print(a);
+# print(a.real);
+# print(a.imag);  
